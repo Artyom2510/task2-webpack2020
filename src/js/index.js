34f77ee860c60966566e4e9daddcf5b89dd34c60
Артём <slavasatane4434343@gmail.com>
2020-09-26 22:47:03 +0300

    template form + jq-datepicker

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,47 @@
+'use strict';
 import '../postcss/main.scss';
 
 import $ from 'jquery';
+import datepickerFactory from 'jquery-datepicker';
+import datepickerJAFactory from 'jquery-datepicker/i18n/jquery.ui.datepicker-ja';
 
-const burger = $('.js-btn-burger');
-const root = $('.js-root');
-const menu = $('.js-menu');
+datepickerFactory($);
+datepickerJAFactory($);
 
-burger.on('click', function() {
-	$(this).children().toggleClass('btn-burger__icon_open btn-burger__icon_close');
-	menu.toggleClass('header__wrap_translate');
-	root.toggleClass('root_overflow');
+$(function() {
+	const burger = $('.js-btn-burger');
+	const root = $('.js-root');
+	const menu = $('.js-menu');
+	// const date = new Date();
+
+	// Бургер в хедере
+	burger.on('click', function() {
+		$(this).children().toggleClass('btn-burger__icon_open btn-burger__icon_close');
+		menu.toggleClass('header__wrap_translate');
+		root.toggleClass('root_overflow');
+	});
+
+	$.datepicker.regional['ru']= {
+		closeText: 'Закрыть',
+		prevText: 'Пред',
+		nextText: 'След',
+		currentText: 'Сегодня',
+		monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+		'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+		monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
+		'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+		dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг',
+		'пятница', 'суббота'],
+		dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+		dayNamesMin: ['Вс' ,'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+		weekHeader: 'Нед',
+		dateFormat: 'dd.mm.yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''
+	};
+	$.datepicker.setDefaults($.datepicker.regional['ru']);
+
+	$('.js-datepicker').datepicker();
 });
